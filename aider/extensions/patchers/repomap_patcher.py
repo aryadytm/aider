86c196c2c4e0155e27eea00693d926a29363ac56
Arya Adyatma <88531676+aryadytm@users.noreply.github.com>
2024-09-17 20:47:49 +0700

    feat: Add support for Swift files in RepoMap

--- aider/extensions/patchers/repomap_patcher.py
+++ aider/extensions/patchers/repomap_patcher.py
@@ -7,45 +7,23 @@ def is_swift_file(filename):
     return filename.lower().endswith('.swift')
 
 
-def patch_render_tree():
-    def patched_render_tree(self, abs_fname, rel_fname, lois):
-        if is_swift_file(rel_fname):
-            with open(abs_fname, 'r') as f:
-                swift_code = f.read()
-            return convert_swift_to_ast(swift_code)
-        return self.original_render_tree(abs_fname, rel_fname, lois)
-
-    RepoMap.original_render_tree = RepoMap.render_tree
-    RepoMap.render_tree = patched_render_tree
-
-def apply_patch():
-    patch_render_tree()
-    
-    def patched_get_ranked_tags_map_uncached(self, *args, **kwargs):
-        original_result = self.original_get_ranked_tags_map_uncached(*args, **kwargs)
+def apply_patch():    
+    def patched_get_ranked_tags_map_uncached(
+        self,
+        chat_fnames,
+        other_fnames=None,
+        max_map_tokens=None,
+        mentioned_fnames=None,
+        mentioned_idents=None,
+    ):
+        all_fnames = []  # TODO: combine chat_fnames and other_fnames
+        tree_str = ""
         
-        if not original_result:
-            return original_result
-
-        swift_files = [tag[0] for tag in original_result if is_swift_file(tag[0])]
+        # TODO: if there are NO swift files, use the original original_get_ranked_tags_map_uncached
         
-        if not swift_files:
-            return original_result
-
-        swift_trees = {}
-        for swift_file in swift_files:
-            with open(os.path.join(self.root, swift_file), 'r') as f:
-                swift_code = f.read()
-            swift_trees[swift_file] = convert_swift_to_ast(swift_code)
-
-        modified_result = []
-        for tag in original_result:
-            if tag[0] in swift_trees:
-                modified_result.append((tag[0], swift_trees[tag[0]]))
-            else:
-                modified_result.append(tag)
-
-        return modified_result
+        # TODO: there are swift file, loop through all_fnames. if is_swift_file(fname), then convert_swift_to_ast(fname) and append to tree_str along with file path above
+        
+        pass
 
     RepoMap.original_get_ranked_tags_map_uncached = RepoMap.get_ranked_tags_map_uncached
     RepoMap.get_ranked_tags_map_uncached = patched_get_ranked_tags_map_uncached
